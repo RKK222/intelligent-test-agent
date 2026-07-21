53dbf9087f748ee17379dc0c484920d69e29b240
guojq <731115882@qq.com>
2026-07-21 15:00:48 +0800

    回复 id

--- backend/test-agent-system-management/src/main/java/com/enterprise/testagent/system/management/user/ThirdPartyUserApiClient.java
+++ backend/test-agent-system-management/src/main/java/com/enterprise/testagent/system/management/user/ThirdPartyUserApiClient.java
@@ -14,7 +14,7 @@ public class ThirdPartyUserApiClient {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ThirdPartyUserApiClient.class);
     private static final String HEADER_TOOL_ID = "toolId";
-    private static final String TOOL_ID_VALUE = "66136bfa5c1c6105572b0118880261d6";
+    private static final String TOOL_ID_VALUE = "66f36bfa5c1c6105572b0118880261d6";
 
     private final ThirdPartyApiProperties properties;
     private final RestTemplate restTemplate;
